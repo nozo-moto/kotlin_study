97655342e3613ee3d017d4d63247de9e24cfd318
nozomi0966 <nozomotoitech@gmail.com>
2018-10-16 15:28:24 +0900

    exposedを導入

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,16 @@
 package code.main
+
 import spark.kotlin.*
+import org.jetbrains.exposed.sql.Database
+import java.sql.Connection
+
 
 fun main(args: Array<String>) {
     val http: Http = ignite()
+    val db = Database.connect("jdbc:mysql://localhost:3308/kotlin_study", driver = "com.mysql.jdbc.Driver", user = "root", password = "mysql")
 
     http.get("/hello") {
         "Hello Spark Kotlin!"
     }
+
 }
